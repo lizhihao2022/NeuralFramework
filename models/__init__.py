b73da1eb6e92f5430a165de9b1810d6d45e893e1
Zhihao Li <lizhihao2022@gmail.com>
2025-12-17 11:28:31 +0800

    Remove SwinTransformer from model registry and exports in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,19 +2,18 @@
 from .mlp import MLP
 from .transformer import Transformer
 from .m2no import M2NO2d
-from .swin_transformer import SwinTransformer, SwinTransformerV2, SwinMLP
+from .swin_transformer import SwinTransformerV2, SwinMLP
 
 
 MODEL_REGISTRY = {
     "MLP": MLP,
     "M2NO2d": M2NO2d,
     "Transformer": Transformer,
-    "SwinTransformer": SwinTransformer,
     "SwinTransformerV2": SwinTransformerV2,
     "SwinMLP": SwinMLP,
 }
 
 __all__ = [
     "MODEL_REGISTRY", 
-    "MLP", "Transformer", "M2NO2d", "SwinTransformer", "SwinTransformerV2", "SwinMLP"
+    "MLP", "Transformer", "M2NO2d", "SwinTransformerV2", "SwinMLP"
     ]
